1d225d40cea5c2a97ea7bcfc48bc8bb5588c3e31
Alexander Fedkin <71890589+qsqnk@users.noreply.github.com>
2022-02-26 22:20:34 +0300

    Time: 0 ms (100.00%), Space: 41.7 MB (20.04%) - LeetHub

--- 100-same-tree/100-same-tree.java
+++ 100-same-tree/100-same-tree.java
@@ -15,14 +15,10 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if (p == null && q == null) {
-            return true;
-        } else if (p != null && q != null) {
-            return p.val == q.val &&
-                isSameTree(p.left, q.left) &&
-                isSameTree(p.right, q.right);
-        } else {
-            return false;
-        }
+        if (p == null || q == null) return p == q;
+        return 
+            p.val == q.val &&
+            isSameTree(p.left, q.left) &&
+            isSameTree(p.right, q.right);
     }
 }
