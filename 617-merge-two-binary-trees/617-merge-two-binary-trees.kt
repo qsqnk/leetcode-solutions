c410abbf8c2632668e47083780bce027c352d49e
Alexander Fedkin <71890589+qsqnk@users.noreply.github.com>
2022-03-01 02:27:05 +0300

    Time: 383 ms (65.90%), Space: 51.9 MB (24.28%) - LeetHub

---
+++ 617-merge-two-binary-trees/617-merge-two-binary-trees.kt
@@ -0,0 +1,20 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun mergeTrees(root1: TreeNode?, root2: TreeNode?): TreeNode? {
+        root1 ?: return root2
+        root2 ?: return root1
+        return TreeNode(root1.`val` + root2.`val`).apply {
+            left = mergeTrees(root1.left, root2.left)
+            right = mergeTrees(root1.right, root2.right)
+        }
+    }
+}
