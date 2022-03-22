62c72b7f240e71615d8d60192c05d971149bf7fd
Alexander Fedkin <71890589+qsqnk@users.noreply.github.com>
2022-03-22 16:39:59 +0300

    Time: 588 ms (33.33%), Space: 28.9 MB (37.04%) - LeetHub

--- 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.py
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.py
@@ -1,6 +1,5 @@
 class Solution:
     def xorQueries(self, arr: List[int], queries: List[List[int]]) -> List[int]:
-        pref = list(itertools.accumulate(arr, operator.xor))
-        get = lambda q: pref[q[1]] ^ (0 if q[0] == 0 else pref[q[0] - 1])
-        return map(get, queries)
+        p = list(accumulate(arr, operator.xor))
+        return map(lambda q: (p[q[1]], p[q[0] - 1] ^ p[q[1]])[q[0] > 0], queries)
         
