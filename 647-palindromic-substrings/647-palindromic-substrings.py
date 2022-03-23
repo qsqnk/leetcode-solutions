045d46717fa9bcf56bca6d4e53722d7055a2dfeb
Alexander Fedkin <71890589+qsqnk@users.noreply.github.com>
2022-03-24 00:59:00 +0300

    Time: 1317 ms (5.01%), Space: 179.5 MB (6.06%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.py
+++ 647-palindromic-substrings/647-palindromic-substrings.py
@@ -2,7 +2,5 @@ class Solution:
     def countSubstrings(self, s: str) -> int:
         @cache
         def isPalindrome(i, j):
-            return True if i >= j else s[i] == s[j] and isPalindrome(i + 1, j - 1)
+            return i >= j or s[i] == s[j] and isPalindrome(i + 1, j - 1)
         return sum(isPalindrome(i, j) for i in range(len(s)) for j in range(i, len(s)))
-            
-        
