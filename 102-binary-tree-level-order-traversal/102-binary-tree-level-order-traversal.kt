5bf23101c58c8d100306238f719022eeab1dea57
Alexander Fedkin <71890589+qsqnk@users.noreply.github.com>
2022-04-11 09:20:47 +0300

    Time: 410 ms (6.50%), Space: 37.8 MB (25.75%) - LeetHub

---
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.kt
@@ -0,0 +1,27 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun levelOrder(root: TreeNode?): List<List<Int>> {
+        root ?: return emptyList()
+        val levels = arrayListOf<List<Int>>()
+        var level = listOf(root)
+        val children = { x: TreeNode -> arrayListOf<TreeNode>().apply {
+                x.left?.let(::add)
+                x.right?.let(::add)
+            }
+        }
+        while (level.isNotEmpty()) {
+            levels.add(level.map { it.`val` }) 
+            level = level.flatMap(children)
+        }
+        return levels
+    }
+}
