a50aa2a40b97b567272c8c7e2c1a028729adb93e
Alexander Fedkin <71890589+qsqnk@users.noreply.github.com>
2022-04-14 16:16:28 +0300

    Time: 210 ms (92.10%), Space: 37.9 MB (90.29%) - LeetHub

--- 1-two-sum/1-two-sum.kt
+++ 1-two-sum/1-two-sum.kt
@@ -2,8 +2,8 @@ class Solution {
     tailrec fun twoSum(A: IntArray,
                        t: Int,
                        ctx: HashMap<Int, Int> = hashMapOf(),
-                       i: Int = 0): IntArray = when {
-        t - A[i] in ctx -> intArrayOf(ctx.getOrDefault(t - A[i], -1), i)
-        else -> twoSum(A, t, ctx.apply { put(A[i], i) }, i + 1)
+                       i: Int = 0): IntArray = when (val j = ctx[t - A[i]]) {
+        null -> twoSum(A, t, ctx.apply { put(A[i], i) }, i + 1)
+        else -> intArrayOf(j, i)
     }
 }
