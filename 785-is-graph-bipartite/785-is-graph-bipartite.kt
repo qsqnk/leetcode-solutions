cd17dee9fdc8977a2cae4e56f3242007ddf68d85
Alexander Fedkin <71890589+qsqnk@users.noreply.github.com>
2022-04-20 13:41:26 +0300

    Time: 485 ms (20.51%), Space: 63.1 MB (10.26%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.kt
+++ 785-is-graph-bipartite/785-is-graph-bipartite.kt
@@ -1,18 +1,20 @@
 class Solution {
     fun isBipartite(g: Array<IntArray>): Boolean {
         val p = IntArray(g.size) { -1 }
-        var bad = false
+        var good = true
         fun dfs(u: Int, even: Boolean) {
             p[u] = if (even) 0 else 1
             for (v in g[u]) {
-                if (p[v] == -1) dfs(v, !even)
-                else if (p[u] == p[v]) bad = true
+                if (p[v] == -1) 
+                    dfs(v, !even)
+                else if (p[u] == p[v]) 
+                    good = false
             }
         }
-        
         for (u in g.indices) {
-            if (p[u] == -1) dfs(u, even=true)
+            if (p[u] == -1) 
+                dfs(u, even=true)
         }
-        return !bad
+        return good
     }
 }
