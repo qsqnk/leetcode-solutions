99a01d3eb08ef78504f4df621261ff9bb5cfbcb5
Alexander Fedkin <71890589+qsqnk@users.noreply.github.com>
2022-06-22 02:53:56 +0300

    Time: 397 ms (30.00%), Space: 45.5 MB (15.63%) - LeetHub

--- 46-permutations/46-permutations.kt
+++ 46-permutations/46-permutations.kt
@@ -1,21 +1,21 @@
 class Solution {
     fun permute(nums: IntArray): List<List<Int>> {
         val passed = hashSetOf<Int>()
-        val current = ArrayDeque<Int>()
-        val permutations = mutableListOf<List<Int>>()
+        val curPerm = ArrayDeque<Int>()
+        val perms = mutableListOf<List<Int>>()
         
-        fun dfs(v: Int) {
+        fun fill(v: Int) {
             passed.add(v)
-            current.add(v)
+            curPerm.add(v)
             
-            if (current.size == nums.size) permutations.add(current.toList())
-            for (u in nums) if (u !in passed) dfs(u)
+            if (curPerm.size == nums.size) perms.add(curPerm.toList())
             
-            current.removeLast()
+            nums.filterNot(passed::contains).forEach(::fill)
+            
+            curPerm.removeLast()
             passed.remove(v)
         }
         
-        nums.forEach(::dfs)
-        return permutations
+        return perms.apply { nums.forEach(::fill) }
     }
 }
